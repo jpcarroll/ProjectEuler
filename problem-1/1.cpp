427dc30b57721327075428ed8f6ba781ba0baa91
jpcarroll <jpcarrrolll@gmail.com>
2019-12-30 14:22:47 -0500

    added problem-2

--- problem-1/1.cpp
+++ problem-1/1.cpp
@@ -2,18 +2,15 @@
 using namespace std;
 
 int find3or5(int n) {
-   int sum = 0;
-   for(int i = 0; i < n; i++) {
-       sum += (i % 5 == 0 || i % 3 == 0) ? i : 0;
-   }
-   return sum; 
+    int sum = 0;
+    for(int i = 0; i < n; i++) {
+        sum += (i % 5 == 0 || i % 3 == 0) ? i : 0;
+    }
+    return sum; 
 }
 
 int main(void) {
-
     int n = 1000;
-
     cout << find3or5(n) << endl;
-
     return 0;
 }
